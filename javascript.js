054e945cf98cb5adbfc34c91a932d9b6cbacf0f5
velasquesmiguel <velasquesjmiguel@gmail.com>
2023-06-22 17:55:46 -0300

    change game function name to round and add more permutations

--- javascript.js
+++ javascript.js
@@ -24,18 +24,31 @@ console.log(getComputerChoice());
 let playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
 console.log(playerChoice)
 
-function game() {
+function round() {
     if (getComputerChoice() === playerChoice) {
         return 'Draw! play again'
     }
-    else if (playerChoice == 'rock' && getComputerChoice() === 'scissors' || playerChoice === 'paper' && getComputerChoice() === 'rock' || playerChoice === 'scissors' && getComputerChoice() === 'paper' ){
-        return "You Win! One Point for you"
+    else if(playerChoice === 'rock' && getComputerChoice() === 'scissors'){
+        return "You Win! Rock beats Scissors"
     }
-    else if (playerChoice == 'rock' && getComputerChoice() === 'paper' || playerChoice === 'paper' && getComputerChoice() === 'scissors' || playerChoice === 'scissors' && getComputerChoice() === 'rock'){
-        return "You Loose! One Point for the Computer"
+    else if(playerChoice === 'paper' && getComputerChoice === 'rock'){
+        return "You Win! Paper beats Rock"
+    }
+    else if(playerChoice === 'scissors' && getComputerChoice() === 'paper'){
+        return "You win! Scissors beats Paper"
+    }
+    else if(playerChoice === 'rock' && getComputerChoice() === 'paper'){
+        return "You Loose! Paper beats Rock"
+    }
+    else if(playerChoice === 'paper' && getComputerChoice() === 'scissors'){
+        return "You Loose! Scissors beats Paper"
+    }
+    else if(playerChoice === 'scissors' && getComputerChoice() === 'rock'){
+        return "You Loose! Rock beats Scissors"
     }
     else {
         return "You must have entered the wrong input!"
     }
 }
-console.log(game())
+console.log(round())
+
