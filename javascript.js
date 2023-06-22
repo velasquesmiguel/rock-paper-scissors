4ba14d2be237de3eb15564bfd236999ddc2aeb05
velasquesmiguel <velasquesjmiguel@gmail.com>
2023-06-22 17:48:23 -0300

    game function added

--- javascript.js
+++ javascript.js
@@ -20,5 +20,22 @@ function getComputerChoice(){
     }
 }
 console.log(getComputerChoice());
-let playerChoice = prompt('Choose Rock, Paper or Scissors').toLowerCase();
+
+let playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
 console.log(playerChoice)
+
+function game() {
+    if (getComputerChoice() === playerChoice) {
+        return 'Draw! play again'
+    }
+    else if (playerChoice == 'rock' && getComputerChoice() === 'scissors' || playerChoice === 'paper' && getComputerChoice() === 'rock' || playerChoice === 'scissors' && getComputerChoice() === 'paper' ){
+        return "You Win! One Point for you"
+    }
+    else if (playerChoice == 'rock' && getComputerChoice() === 'paper' || playerChoice === 'paper' && getComputerChoice() === 'scissors' || playerChoice === 'scissors' && getComputerChoice() === 'rock'){
+        return "You Loose! One Point for the Computer"
+    }
+    else {
+        return "You must have entered the wrong input!"
+    }
+}
+console.log(game())
