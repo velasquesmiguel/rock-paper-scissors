0651266dfc59dcf21e3dc5221778c0eca8cb370d
velasquesmiguel <velasquesjmiguel@gmail.com>
2023-06-23 02:24:16 -0300

    game running for 1 round

--- javascript.js
+++ javascript.js
@@ -8,47 +8,59 @@
 4 - Repeat the game 5 times
 5 - Tally the total wins/losses and declare a winner between the computer or human
 */
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     const compChoice = Math.floor(Math.random() * 3);
-    if (compChoice == 0) {
+    if (compChoice === 0) {
     return 'rock';
-    } else if (compChoice == 1) {
+    } else if (compChoice === 1) {
     return 'paper'
     } else {
     return 'scissors'
     }
 }
-console.log(getComputerChoice());
 
-let playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
-console.log(playerChoice)
+
+const playerChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+const computerChoice = getComputerChoice();
+console.log("computer choice is", computerChoice)
+console.log("player choice is", playerChoice);
+
 
 function round() {
-    if (getComputerChoice() === playerChoice) {
+    if (computerChoice === playerChoice) {
         return 'Draw! play again'
     }
-    else if(playerChoice === 'rock' && getComputerChoice() === 'scissors'){
-        return "You Win! Rock beats Scissors"
+    else if(playerChoice === 'rock' && computerChoice === 'scissors'){
+        playerScore++
+        return "You Win! Rock beats Scissors." 
     }
-    else if(playerChoice === 'paper' && getComputerChoice === 'rock'){
-        return "You Win! Paper beats Rock"
+    else if(playerChoice === 'paper' && computerChoice === 'rock'){
+        playerScore++
+        return "You Win! Paper beats Rock."
     }
-    else if(playerChoice === 'scissors' && getComputerChoice() === 'paper'){
-        return "You win! Scissors beats Paper"
+    else if(playerChoice === 'scissors' && computerChoice === 'paper'){
+        playerScore++
+        return "You win! Scissors beats Paper."
     }
-    else if(playerChoice === 'rock' && getComputerChoice() === 'paper'){
-        return "You Loose! Paper beats Rock"
+    else if(playerChoice === 'rock' && computerChoice === 'paper'){
+        computerScore++
+        return "You Loose! Paper beats Rock."
     }
-    else if(playerChoice === 'paper' && getComputerChoice() === 'scissors'){
-        return "You Loose! Scissors beats Paper"
+    else if(playerChoice === 'paper' && computerChoice === 'scissors'){
+        computerScore++
+        return "You Loose! Scissors beats Paper."
     }
-    else if(playerChoice === 'scissors' && getComputerChoice() === 'rock'){
-        return "You Loose! Rock beats Scissors"
+    else if(playerChoice === 'scissors' && computerChoice === 'rock'){
+        computerScore++
+        return "You Loose! Rock beats Scissors."
     }
     else {
         return "You must have entered the wrong input!"
     }
 }
 console.log(round())
-
+console.log(playerScore)
+console.log(computerScore)
